6aa55178d9f833aa9eb0e235ede7cc36a969e71e
Pierddev <139257364+Pierddev@users.noreply.github.com>
2025-12-05 13:56:18 +0100

    2. Le header
    - Passage de .html > .php pour l'index
    - Ajout des 2 boutons de la navbar pour la navigation entre les sections

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+   <title>DUAULT Pierre - Devoir</title>
+</head>
+<body class="h-[200vh] bg-[#00141a] text-white">
+   <header class="flex flex-col items-center w-full py-2">
+      <nav class="flex gap-10 text-lg bg-white/10 p-2 rounded-xl">
+         <a class="hover:bg-white/10 rounded-lg p-2" href="#accueil">Accueil</a>
+         <a class="hover:bg-white/10 rounded-lg p-2" href="#contact">Contactez-moi</a>
+      </nav>
+   </header>
+</body>
+</html>
